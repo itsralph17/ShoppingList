e296fbb15edabc3b3470f7b6e2e64931e46ed6e8
itsralph17 <rapraku@appdev.yearup.org>
2023-04-12 11:46:15 -0400

    adding new files

---
+++ src/main/java/com/learntocode/ShoppingList.java
@@ -0,0 +1,17 @@
+package com.learntocode;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        System.out.println("3Apples");
+        System.out.println("32oz Almond Milk");
+        System.out.println("10 pounds Jasmine Rice ");
+        System.out.println("3 potatoes");
+        System.out.println("Tide liquid detergent 1 gallon");
+        System.out.println("frozen whole chicken ");
+        System.out.println("Bbq sauce");
+        System.out.println("24 pack poland spring water ");
+        System.out.println("12 pack pepsi");
+        System.out.println("1 litre sprite");
+
+    }
+}
